1a239f3d517a8d5abe6757d653b511ed5cd6bb0a
Tianjian <tianjianqin@outlook.com>
2024-06-19 12:44:37 +0200

    Include MLE typical and optimal cases in the bootstrapping function

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -3,14 +3,48 @@ estimation_bootstrap <- function(estimate, scenario = "DDD", n = 100) {
   if (scenario != "DDD") {
     stop("Only DDD is supported for boostrapping")
   }
+  message("There are ", n, " bootstrap iterations")
 
-  results <- data.frame(lambda = numeric(n), mu = numeric(n), cap = numeric(n), stringsAsFactors = FALSE)
+  results_NN <- data.frame(lambda = numeric(n), mu = numeric(n), cap = numeric(n), stringsAsFactors = FALSE)
+  results_ML_Typ <- data.frame(lambda = numeric(n), mu = numeric(n), cap = numeric(n), stringsAsFactors = FALSE)
+  results_ML_Opt <- data.frame(lambda = numeric(n), mu = numeric(n), cap = numeric(n), stringsAsFactors = FALSE)
   for (i in seq_len(n)) {
-    tree <- dd_sim(c(estimate$pred_lambda, estimate$pred_mu, estimate$pred_cap), age = 10, ddmodel = 1)$tes
-    result <- suppressMessages(estimate_from_simulation(tree))
+    sim <- dd_sim(c(estimate$pred_lambda, estimate$pred_mu, estimate$pred_cap), age = 10, ddmodel = 1)
+    tree <- sim$tes
+    brts <- sim$brts
+    result_NN <- suppressMessages(estimate_from_simulation(tree))
+    result_ML_Typ <- suppressMessages(dd_ML(
+      brts = brts,
+      initparsopt = c(runif(1, 0.1, 4), runif(1, 0, 1.5), runif(1, 10, 1000)),
+      idparsopt = c(1, 2, 3),
+      btorph = 0,
+      soc = 2,
+      cond = 1,
+      ddmodel = 1,
+      num_cycles = Inf,
+      optimmethod = 'simplex'
+    ))
+    result_ML_Opt <- suppressMessages(dd_ML(
+      brts = brts,
+      initparsopt = c(estimate$pred_lambda, estimate$pred_mu, estimate$pred_cap),
+      idparsopt = c(1, 2, 3),
+      btorph = 0,
+      soc = 2,
+      cond = 1,
+      ddmodel = 1,
+      num_cycles = Inf,
+      optimmethod = 'simplex'
+    ))
+
+    message("Computing from neural network...")
+    results_NN[i, ] <- c(result$pred_lambda, result$pred_mu, result$pred_cap)
+    message("Computing from ML with typical starting values...")
+    results_ML_Typ[i, ] <- c(result_ML_Typ$lambda, result_ML_Typ$mu, result_ML_Typ$K)
+    message("Computing from ML with optimal starting values...")
+    results_ML_Opt[i, ] <- c(result_ML_Opt$lambda, result_ML_Opt$mu, result_ML_Opt$K)
     message("Bootstrap iteration ", i, " completed")
-    results[i, ] <- c(result$pred_lambda, result$pred_mu, result$pred_cap)
   }
 
-  return(results)
+  out <- list(results_NN = results_NN, results_ML_Typ = results_ML_Typ, results_ML_Opt = results_ML_Opt)
+  return(out)
 }
